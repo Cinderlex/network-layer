efcb3922ad93ffba8f10ff8c90baf6aed8456a5f
Cinderlex <cinderlex@pm.me>
2023-11-17 15:05:45 +0300

    Added formatting and linting checks

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -1,7 +1,8 @@
-import {Either, left, right} from './Either';
+import { Either, left, right } from './Either';
 
 type BodyInit2 = XMLHttpRequestBodyInit;
 type BodyInit2Chromium = ReadableStream | XMLHttpRequestBodyInit;
+
 interface RequestInitBase {
     cache?: RequestCache;
     credentials?: RequestCredentials;
@@ -16,15 +17,23 @@ interface RequestInitBase {
     signal?: AbortSignal | null;
     window?: null;
 }
+
 interface RequestInit2 extends RequestInitBase {
     body?: BodyInit2 | null;
 }
+
 interface RequestInit2Chromium extends RequestInitBase {
     body?: BodyInit2Chromium | null;
 }
 
 export type Fetch2 = (input: RequestInfo | URL, init?: RequestInit2) => Promise<Either<unknown, Response>>;
 export const fetch2: Fetch2 = (input, init?) =>
-    fetch(input, init as RequestInit | undefined).then(result => right(result), error => left(error));
-export type Fetch2Chromium = (input: RequestInfo | URL, init?: RequestInit2Chromium) => Promise<Either<unknown, Response>>;
+    fetch(input, init as RequestInit | undefined).then(
+        result => right(result),
+        error => left(error)
+    );
+export type Fetch2Chromium = (
+    input: RequestInfo | URL,
+    init?: RequestInit2Chromium
+) => Promise<Either<unknown, Response>>;
 export const fetch2Chromium = fetch2 as Fetch2Chromium;
--- packages/fetch/vite.config.ts
+++ packages/fetch/vite.config.ts
@@ -1,18 +1,18 @@
-import path from 'path';
-import {defineConfig} from 'vite';
+import { resolve as resolvePath } from 'path';
+import { defineConfig } from 'vite';
 import dtsPlugin from 'vite-plugin-dts';
 
 export default defineConfig({
     build: {
         outDir: 'lib',
         lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
+            entry: resolvePath(__dirname, 'src/index.ts'),
             formats: ['es', 'cjs'],
-            fileName: (format) => {
-                const names = {es: 'index.mjs', cjs: 'index.js'};
+            fileName: format => {
+                const names = { es: 'index.mjs', cjs: 'index.js' };
                 return names[format];
             },
         },
     },
-    plugins: [dtsPlugin({rollupTypes: true})],
+    plugins: [dtsPlugin({ rollupTypes: true })],
 });
